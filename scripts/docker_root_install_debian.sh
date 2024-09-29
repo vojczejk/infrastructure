adc7105e1d69560345a6f027da6288af7794e597
Vojtěch Nevřela <nevrelav@gmail.com>
2024-09-29 23:26:36 +0200

    Fix docker install scripts

--- scripts/docker_root_install_debian.sh
+++ scripts/docker_root_install_debian.sh
@@ -1,8 +1,8 @@
 # Adapten from https://docs.docker.com/engine/install/debian/
 
 # Add Docker's official GPG key:
-apt update
-apt install ca-certificates curl
+apt-get update
+apt-get install ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -12,6 +12,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+apt-get update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Install docker
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
